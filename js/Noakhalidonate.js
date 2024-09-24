ea00cb0f1a634bafa69dafa79542d182f6d62dd5
Masudrana95 <masudranabappy33@gmail.com>
2024-09-24 23:12:59 +0600

    function js add and some code add

--- js/Noakhalidonate.js
+++ js/Noakhalidonate.js
@@ -1,45 +1,38 @@
 document.getElementById('btn-donate')
 .addEventListener('click', function(event){
     event.preventDefault();
-    const addMoney = document.getElementById('input-donate').value;
-    const addMoneyNumber = parseFloat (addMoney);
-    const balance = document.getElementById('donAm').innerText
-    const balanceNumber = parseFloat(balance);
-    const newBalance = balanceNumber + addMoneyNumber;
-    const totalBalance = document.getElementById('account-balance').innerText
-    const totalBalanceNumber = totalBalance - addMoneyNumber;
-    document.getElementById('account-balance').innerText = totalBalanceNumber;
+    const addMoney = getInputeFieldValueById ('input-donate');
+    const balance = getTextFieldValueById ('donAm');
+    const addMoneyNumber = getTextFieldValueById ('account-balance');
+    const newBalance = addMoney + balance;
+    const totalBalance = addMoneyNumber - addMoney;
+    document.getElementById('account-balance').innerText = totalBalance;
     document.getElementById('donAm').innerText = newBalance;
+    
 })
 
 document.getElementById('btn-feni-donate')
 .addEventListener('click', function(event){
     event.preventDefault();
-    const addMoney = document.getElementById('input-feni-donate').value;
-    const addMoneyNumber = parseFloat (addMoney);
-    const balance = document.getElementById('fenidonAm').innerText
-    const balanceNumber = parseFloat(balance);
-    const newBalance = balanceNumber + addMoneyNumber;
-    const totalBalance = document.getElementById('account-balance').innerText
-    const totalBalanceNumber = totalBalance - addMoneyNumber;
-    document.getElementById('account-balance').innerText = totalBalanceNumber;
+    const addMoney = getInputeFieldValueById ('input-feni-donate');
+    const balance = getTextFieldValueById ('fenidonAm');
+    const addMoneyNumber = getTextFieldValueById ('account-balance');
+    const newBalance = addMoney + balance;
+    const totalBalance = addMoneyNumber - addMoney;
+    document.getElementById('account-balance').innerText = totalBalance;
     document.getElementById('fenidonAm').innerText = newBalance;
+    
 })
-
 
 document.getElementById('btn-aid-donate')
 .addEventListener('click', function(event){
     event.preventDefault();
-    const addMoney = document.getElementById('input-aid-donate').value;
-    const addMoneyNumber = parseFloat (addMoney);
-    const balance = document.getElementById('aiddonAm').innerText
-    const balanceNumber = parseFloat(balance);
-    const newBalance = balanceNumber + addMoneyNumber;
-    const totalBalance = document.getElementById('account-balance').innerText
-    const totalBalanceNumber = totalBalance - addMoneyNumber;
-    document.getElementById('account-balance').innerText = totalBalanceNumber;
+    const addMoney = getInputeFieldValueById ('input-aid-donate');
+    const balance = getTextFieldValueById ('aiddonAm');
+    const addMoneyNumber = getTextFieldValueById ('account-balance');
+    const newBalance = addMoney + balance;
+    const totalBalance = addMoneyNumber - addMoney;
+    document.getElementById('account-balance').innerText = totalBalance;
     document.getElementById('aiddonAm').innerText = newBalance;
     
 })
-
-
--- js/function.js
+++ js/function.js
@@ -1,8 +1,21 @@
-console.log('ths is a file');
 
+// function getInputeFieldValueById(){
+//     // console.log('this is a function file');
+//     const addMoney = document.getElementById('input-donate').value;
+//     return addMoney;
 
+// }
 
+function getInputeFieldValueById(id){
+    // console.log('this is a function file');
+    const addMoney = document.getElementById(id).value;
+    const addMoneyNumber = parseFloat (addMoney);
+    return addMoneyNumber;
 
-function getInputeFieldValueById(){
-    console.log('this is a function file');
+}
+function getTextFieldValueById(id){
+    const textValue = document.getElementById(id).innerText;
+    const textNumber = parseFloat(textValue);
+    return textNumber;
+
 }
